984ca217528e2e3d4187d341e3ad1b2e12e1cb75
Julian Espinel <julianespinel@gmail.com>
2018-05-01 19:54:14 -0500

    Add unit test to saveStocks method.

---
+++ bvc/persistence_test.go
@@ -0,0 +1,33 @@
+package bvc
+
+import (
+	"testing"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/stretchr/testify/assert"
+	"github.com/julianespinel/mila-api/models"
+)
+
+func getDatabaseConnection(t *testing.T) *gorm.DB {
+	db, err := gorm.Open("sqlite3", "/tmp/miladb_test.db")
+	assert.Nil(t, err)
+	return db
+}
+
+func dropStocksTable(db *gorm.DB) {
+	db.DropTable(&models.Stock{})
+}
+
+func TestBVCPersistence_SaveStocks(t *testing.T) {
+	// Setup
+	db := getDatabaseConnection(t)
+	size := 5
+	stocks := GetTestingStocks(size)
+	// Exercise
+	bvcPersistence := Persistence{db: db}
+	err := bvcPersistence.SaveStocks(stocks)
+	assert.Nil(t, err)
+	// Tear down
+	dropStocksTable(db)
+	db.Close()
+}
