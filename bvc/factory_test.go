18f3cfdb9268be4e2ee1abf3a2aeccebd9abe22c
Julian Espinel <julianespinel@gmail.com>
2018-05-01 19:53:36 -0500

    Add file to handle testing factories.

---
+++ bvc/factory_test.go
@@ -0,0 +1,47 @@
+package bvc
+
+import (
+	"github.com/Pallinder/go-randomdata"
+	"github.com/shopspring/decimal"
+	"time"
+	"github.com/julianespinel/mila-api/models"
+)
+
+func getRandomString() string {
+	return randomdata.Letters(5)
+}
+
+func getRandomDecimal(initial int, final int) decimal.Decimal {
+	randomFloat := randomdata.Decimal(initial, final)
+	return decimal.NewFromFloat(randomFloat)
+}
+
+func getRandomInt(initial int, final int) int64 {
+	return int64(randomdata.Number(initial, final))
+}
+
+func GetRandomStock() models.Stock {
+	closePrice := getRandomDecimal(2, 6)
+	stock := models.Stock{
+		Date:     time.Now(),
+		Symbol:   getRandomString(),
+		Name:     getRandomString(),
+		Currency: getRandomString(),
+		Open:     getRandomDecimal(0, 3),
+		High:     getRandomDecimal(3, 6),
+		Low:      getRandomDecimal(0, 2),
+		Close:    closePrice,
+		AdjClose: closePrice,
+		Volume:   getRandomInt(10, 100),
+	}
+	return stock
+}
+
+func GetTestingStocks(size int) []models.Stock {
+	stocks := make([]models.Stock, size)
+	for i := 0; i < size; i++ {
+		stock := GetRandomStock()
+		stocks = append(stocks, stock)
+	}
+	return stocks
+}
