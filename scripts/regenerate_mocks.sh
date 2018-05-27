dd58a93755dfc9665c6457890064e605742e8d83
Julian Espinel <julianespinel@gmail.com>
2018-05-27 15:33:24 -0500

    Update packages.

--- scripts/regenerate_mocks.sh
+++ scripts/regenerate_mocks.sh
@@ -1,4 +1,4 @@
 cd ..
-mockgen -source=bvc/domain.go -destination=bvc/mock_domain.go -package=bvc
+mockgen -source=core/domain.go -destination=core/mock_domain.go -package=core
+mockgen -source=core/persistence.go -destination=core/mock_persistence.go -package=core
 mockgen -source=bvc/client.go -destination=bvc/mock_client.go -package=bvc
-mockgen -source=bvc/persistence.go -destination=bvc/mock_persistence.go -package=bvc
